78b542c308b8557ae36b97d1a9c6fad25384ebd6
Adam Stiavetti <adam.r.stiavetti.mil@army.mil>
2025-02-11 13:44:30 -0600

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner obj = new Scanner(System.in);
+        int x = obj.nextInt();
+
+        switch(x){
+            case 1 : System.out.println("Hello");
+                break;
+            case 2: System.out.println("world");
+                break;
+            case 3: System.out.println("ONE MORE TIME");
+                break;
+        }
     }
 }
